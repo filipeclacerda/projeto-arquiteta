719aae2634c816adf93d8963058b2fd02f7ed846
Filipe Lacerda <filipeclacerda@gmail.com>
2023-04-02 19:14:13 -0300

    contact screen done / some refactoring

--- js/router.js
+++ js/router.js
@@ -2,7 +2,7 @@ const route = (event) => {
   event = event || window.event;
   event.preventDefault();
   window.history.pushState({}, "", event.target.href);
-  handleLocation();
+  handleLocationChange();
 };
 
 const routes = {
@@ -13,19 +13,25 @@ const routes = {
   "/contato": "/pages/contato.html",
 };
 
-const handleLocation = async () => {
+const handleLocationChange = async () => {
   const path = window.location.pathname;
   const route = routes[path] || routes[404];
   const html = await fetch(route).then((data) => data.text());
   document.getElementById("main-page").innerHTML = html;
   const scriptName = path == "/" ? "/home.js" : `${path}.js`;
   $("#page-script").load(`/js${scriptName}`);
-  $("#navbar").load("../pages/navbar.html");
-  $("#footer").load("../pages/footer.html");
+  removeActive();
+  addActive();
   window.scrollTo(0, 0);
 };
 
-window.onpopstate = handleLocation;
+window.addEventListener("popstate", handleLocationChange);
 window.route = route;
 
-handleLocation();
+function removeActive() {
+  $(".menu-item").removeClass("active");
+}
+function addActive() {
+  $(`.menu-item[href="${window.location.pathname}"]`).addClass("active");
+}
+handleLocationChange();
