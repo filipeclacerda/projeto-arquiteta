659144cae74e69816f056beca095e0bbf9de3d3f
Filipe Lacerda <filipe.lacerda@caed.ufjf.br>
2023-04-04 16:03:59 -0300

    images about tab

--- js/sobre.js
+++ js/sobre.js
@@ -27,3 +27,39 @@ function moveSlide() {
 $(window).resize(function () {
   moveSlide();
 });
+
+let currentProjectIndex = 0;
+let currentCategory = "";
+
+function renderProject() {
+  const project = projects[currentProjectIndex];
+  if (currentCategory && project.categoria !== currentCategory) {
+    // skip this project if it doesn't match the current category filter
+    currentProjectIndex +=
+      currentProjectIndex < projects.length - 1 ? 1 : -currentProjectIndex;
+    renderProject();
+    return;
+  }
+
+  const projectImages = document.getElementById("project-images-min");
+
+  // clear previous images
+  projectImages.innerHTML = "";
+
+  // add new images
+  for (let i = 0; i< project.imagens.length; i++) {
+    const div = document.createElement("div");
+    const image = document.createElement("img");
+    div.classList.add("project-image-min-container");
+    div.setAttribute("data-before", project.nome + " // " + project.categoria);
+    div.appendChild(image);
+    image.classList.add("project-image-min");
+    image.src = project.imagens[i];
+    projectImages.appendChild(div);
+    if(i == 3){
+      break;
+    }
+  }
+}
+
+renderProject();
